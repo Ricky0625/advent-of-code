6893f41fb673906619a3c3e7613518668be8a90d
Ricky Wong <rickywong0625@gmail.com>
2023-12-06 23:08:05 +0800

    fix: day1

--- 2023/Day1/Trebuchet?!/Part2/srcs/trebuchet.cpp
+++ 2023/Day1/Trebuchet?!/Part2/srcs/trebuchet.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
 
 #define INPUT_FILE "../input.txt"
+typedef std::unordered_map<std::string, int> DigitMap;
 
+/**
+ * @brief Convert an ASCII character to its actual numeric form
+ */
 int toDigit(char ch)
 {
     return ch - 48;
@@ -11,11 +16,42 @@ int toDigit(char ch)
 
 void pushNums(std::string line, std::vector<int> &nums)
 {
+    DigitMap digits{
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+    };
+    const size_t MIN_KEY_LEN = 3;
+    const size_t MAX_KEY_LEN = 5;
+
+    std::string subStr;
+
     nums.clear();
-    for (size_t i = 0; i < line.size(); i++)
+    for (std::string::iterator it = line.begin(); it != line.end(); it++)
     {
-        if (isdigit(line[i]))
-            nums.push_back(toDigit(line[i]));
+        if (isdigit(*it))
+        {
+            nums.push_back(toDigit(*it));
+            continue;
+        }
+
+        for (size_t i = MIN_KEY_LEN; i <= MAX_KEY_LEN && it + i <= line.end(); i++)
+        {
+            subStr = std::string(it, it + i);
+            DigitMap::iterator occurrence = digits.find(subStr);
+            if (occurrence != digits.end())
+            {
+                nums.push_back(occurrence->second);
+                break;
+            }
+        }
     }
 }
 
@@ -26,7 +62,7 @@ void pushNums(std::string line, std::vector<int> &nums)
  * lines of text. On each line, the calibration value can be found
  * by combining the first digit and last digit to form a single
  * two-digit number.
- * So the idea is, push the digit into a vector and access the 
+ * So the idea is, push the digit into a vector and access the
  * first and last digit.
  * First: index 0 * 10
  * Last: index (len - 1)
